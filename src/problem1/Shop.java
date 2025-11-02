e6275f11c25c1b63cdeb037f6f52abdf2e5f9e83
YassineZitouni29 <158048035+YassineZitouni29@users.noreply.github.com>
2025-11-02 20:23:21 +0100

    Update Shop.java

--- src/problem1/Shop.java
+++ src/problem1/Shop.java
@@ -6,11 +6,12 @@ public class Shop
 {
     public static void main (String[] args)
     {
-        ArrayList<Item> cart = new ArrayList();
-        Item item;
+        ArrayList<Item> cart = new ArrayList<>();
         String itemName;
         double itemPrice;
         int quantity;
+        double price = 0;
+
         Scanner scan = new Scanner(System.in);
         String keepShopping = "y";
         do
@@ -21,8 +22,17 @@ public class Shop
             itemPrice = scan.nextDouble();
             System.out.print ("Enter the quantity: ");
             quantity = scan.nextInt();
-// *** create a new item and add it to the cart
+            // *** create a new item and add it to the cart
+            Item item = new Item(itemName, itemPrice, quantity);
+            cart.add(item);
+            price+=itemPrice*quantity;
             // *** print the contents of the cart object using println
+            for (int i = 0; i<cart.size(); i++){
+                Item temp =  cart.get(i);
+                System.out.printf("Item: %s , price: %.2f , quantity: %d\n", temp.getName(), temp.getPrice(), temp.getQuantity());
+            }
+            System.out.printf("The total price is : %.2f\n", price);
+            scan.nextLine();
             System.out.print ("Continue shopping (y/n)? ");
             keepShopping = scan.nextLine();
         }
