cfd391e37d53c474f0c41915d6c04adb21a17dab
YassineZitouni29 <158048035+YassineZitouni29@users.noreply.github.com>
2025-11-02 22:49:38 +0100

    Add files via upload

---
+++ src/challenge/Place.java
@@ -0,0 +1,23 @@
+package test;
+class Place {
+    private String name;
+    private int distanceSydney;
+
+    public Place(String name, int distanceSydney) {
+        this.name = name;
+        this.distanceSydney = distanceSydney;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistanceSydney() {
+        return distanceSydney;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " +name + ":" + distanceSydney + "km (from Sydney)]";
+    }
+}
